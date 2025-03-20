24b9cb3b724d0d0670facfd7fb04ea250840b84f
preetiDatacode1 <preetisharma.datacode@gmail.com>
2025-03-20 14:54:34 +0530

    Merge GitHub and local projects

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config();
 const express = require('express');
 const connectDatabase = require('./config/db');
@@ -31,34 +30,4 @@ const startServer = async () => {
   }
 };
 
-=======
-require('dotenv').config();
-const express = require('express');
-const connectDatabase = require('./config/db');
-const productRoutes = require('./routes/productRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/api', productRoutes);
-app.use('/api', cartRoutes); 
-
-// Start Server
-const startServer = async () => {
-  try {
-    await connectDatabase();
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  } catch (error) {
-    console.error('Server error:', error);
-  }
-};
-
->>>>>>> 5417ad366a3d5078390286ca8c87b84794fbd974
 startServer();
